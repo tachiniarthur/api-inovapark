98ba57d9c8929480992290c72cb755e048eb52f6
tachini <tachiniarthur@gmail.com>
2025-03-27 17:31:14 -0300

    second commit

---
+++ src/main/java/br/com/inovapark/api_inovapark/ApiInovaparkApplication.java
@@ -0,0 +1,13 @@
+package br.com.inovapark.api_inovapark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInovaparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInovaparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/inovapark/api_inovapark/ApiInovaparkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.inovapark.api_inovapark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInovaparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
